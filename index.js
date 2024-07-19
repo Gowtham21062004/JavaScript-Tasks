5d9deeffabf670646e43289a73c64d3fb6652034
Gowtham21062004 <142236401+Gowtham21062004@users.noreply.github.com>
2024-07-19 17:09:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+///19/07/2024;
+
+///mappinng
+// let arr=[1,2,3,4,5,6];
+// let res=arr.map(arr=>(arr*5));
+// console.log(res);
+
+///filtering
+// let str=["gowtham","kathir","raja","Gujarathtitans"];
+// let res=str.filter(str=>(str.length>6));
+// console.log(res);
+
+//find
+// let str=["gowtham","RCB","CSK","MI","KKR","SRH","Gujarathtitans"];
+// let res=str.find(str=>(str.length>7));
+// console.log(res)
+
+//merging two arrays
+// let arr1=[2,1,3,4,5,18,7]
+// let arr2=[4,6,7];
+// console.log(...arr1,...arr2);
+
+//rotating an array
+// const arr = [1,2,3,4,5];
+// const res = arr.map((a, i) => arr[arr.length-i-1])
+// console.log(res);
+
+
+
+
